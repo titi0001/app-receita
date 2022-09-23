b17c4d4687ea86fc23e874705bb2a74e38ed855b
Leticia Oliveira <oliveira01e@hotmail.com>
2022-09-23 20:48:25 -0300

    feat: reqs 2 a 4

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Desenvolva testes para atingir cobertura total do APP', () => {
+  test('Verifica se o input E-mail é renderizado', () => {
+    renderWithRouter(<App />);
+
+    const emailRender = screen.getByTestId('email-input');
+
+    expect(emailRender).toBeInTheDocument();
+  });
+
+  test('Verifica se o input Password é renderizado', () => {
+    renderWithRouter(<App />);
+
+    const passwordRender = screen.getByTestId('password-input');
+
+    expect(passwordRender).toBeInTheDocument();
+  });
+  test('Verifica se o botão é renderizado', () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByRole('button', { name: /Enter/i });
+
+    expect(button).toBeInTheDocument();
+  });
+  test('Verifica se button esta desabilitado ao entrar na tela', () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByRole('button', { name: /Enter/i });
+
+    expect(button).toBeDisabled();
+  });
+  test('Verifica se button esta habilitado ao digitar email e senha', () => {
+    renderWithRouter(<App />);
+
+    const buttonHab = screen.getByRole('button', { name: /enter/i });
+    const emailLabel = screen.getByTestId('email-input');
+    const passwordLabel = screen.getByTestId('password-input');
+    const emailtest = 'test@test.com';
+    const senhatest = '0987654';
+
+    userEvent.type(emailLabel, emailtest);
+    userEvent.type(passwordLabel, senhatest);
+
+    expect(buttonHab).toBeEnabled();
+  });
 });
+
