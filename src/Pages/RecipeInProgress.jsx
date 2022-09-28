4a738c61ebe17f39b3952a71e20b0e5367da55b5
Philip Lages <psilvaelages@gmail.com>
2022-09-28 19:37:08 -0300

    feat: add requirements 37, 38 and 39

--- src/Pages/RecipeInProgress.jsx
+++ src/Pages/RecipeInProgress.jsx
@@ -1,11 +1,124 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipeInProgressCard from '../Components/RecipeInProgressCard';
+import RecipesContext from '../Context';
 
 function RecipeInProgress({ match: { params: { id } } }) {
-  console.log(id);
+  const [food, setFood] = useState([]);
+  const [drink, setDrink] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { history: { location: { pathname } } } = useContext(RecipesContext);
+
+  const getApiEAT = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const { meals } = await response.json();
+    return meals;
+  };
+
+  const getApiDrink = async () => {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const { drinks } = await response.json();
+    return drinks;
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      if (pathname.includes('meals')) {
+        const mealData = await getApiEAT();
+        setFood(mealData);
+        setLoading(false);
+      } if (pathname.includes('drinks')) {
+        const drinkData = await getApiDrink();
+        setDrink(drinkData);
+        setLoading(false);
+      }
+    };
+    getData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getIngredients = () => {
+    if (pathname.includes('meals')) {
+      const ingredients = Object.entries(food[0])
+        .filter((item) => item[0].includes('Ingredient'))
+        .filter((ingredient) => ingredient[1] !== null)
+        .filter((ingredient) => ingredient[1] !== '');
+
+      const measures = Object.entries(food[0])
+        .filter((item) => item[0].includes('Measure'))
+        .filter((measure) => measure[1] !== null)
+        .filter((measure) => measure[1] !== '');
+
+      const ingredientsAndMeasures = [];
+
+      ingredients.forEach((ingredient, idx) => {
+        ingredientsAndMeasures.push([...ingredient, measures[idx][1]]);
+      });
+
+      return ingredientsAndMeasures;
+    }
+    if (pathname.includes('drinks')) {
+      const ingredients = Object.entries(drink[0])
+        .filter((item) => item[0].includes('Ingredient'))
+        .filter((ingredient) => ingredient[1] !== null)
+        .filter((ingredient) => ingredient[1] !== '');
+
+      const measures = Object.entries(drink[0])
+        .filter((item) => item[0].includes('Measure'))
+        .filter((measure) => measure[1] !== null)
+        .filter((measure) => measure[1] !== '');
+
+      const ingredientsAndMeasures = [];
+      const firstIngredient = [
+        [...ingredients[0], measures[0][1]],
+        [...ingredients[1], ''],
+        [...ingredients[2], ''],
+      ];
+
+      if (measures.length === 1) {
+        ingredientsAndMeasures.push(...firstIngredient);
+      } else {
+        ingredients.forEach((ingredient, idx) => {
+          ingredientsAndMeasures.push([...ingredient, measures[idx][1]]);
+        });
+      }
+
+      return ingredientsAndMeasures;
+    }
+  };
   return (
-    <RecipeInProgressCard />
+    <section>
+      <h1>Receita em andamento</h1>
+      {!loading && (
+        <section>
+          {pathname.includes('meals') && (
+            <RecipeInProgressCard
+              recipe={ food[0] }
+              func={ getIngredients }
+              pathname={ pathname }
+              thumb="strMealThumb"
+              name="strMeal"
+              category="strCategory"
+              instructions="strInstructions"
+            />
+          )}
+          {pathname.includes('drinks') && (
+            <RecipeInProgressCard
+              recipe={ drink[0] }
+              func={ getIngredients }
+              pathname={ pathname }
+              thumb="strDrinkThumb"
+              name="strDrink"
+              category="strCategory"
+              instructions="strInstructions"
+              alcoholic="strAlcoholic"
+            />
+          )}
+        </section>
+      )}
+
+    </section>
   );
 }
 
