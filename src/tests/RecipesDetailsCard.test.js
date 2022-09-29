22953e1e582191d2a66af0c6a48af7a3fd1feed5
Leticia Oliveira <oliveira01e@hotmail.com>
2022-09-29 17:18:54 -0300

    teste em andamento

--- src/tests/RecipesDetailsCard.test.js
+++ src/tests/RecipesDetailsCard.test.js
@@ -1,34 +1,96 @@
-// import React from 'react';
-// import { screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './RenderWithRouter';
-// import RecipeDetailsCard from '../Components/RecipeDetailsCard';
-// import oneMeal from '../../cypress/mocks/oneMeal';
-// import App from '../App';
-// import fetch from '../../cypress/mocks/fetch';
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import App from '../App';
+import fetch from '../../cypress/mocks/fetch';
+// import meals from '../../cypress/mocks/meals';
 
 describe('Testa 45% do componente RecipeDetailsCard', () => {
-  // afterEach(() => fetchMock.restore(testDoneRecipes));
+  it('Verifica se o botão é rederizado na tela de meals', async () => {
+    const urlMeal = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771';
+    fetch(urlMeal);
 
-  // it.only('Verifica se o botão é rederizado na tela', async () => {
+    const { history } = renderWithRouter(<App />, '/meals/52771');
 
-  //   global.fetch = jest.fn().mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue(oneMeal),
-  //   });
-  //   renderWithRouter(<App />, '/meals/52771');
+    const startButton = screen.getByTestId('start-recipe-btn');
+    const favButton = screen.getByTestId('favorite-btn');
+    const shareButton = screen.getByTestId('share-btn');
 
-  //   const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=52771';
+    expect(startButton).toBeInTheDocument();
+    expect(favButton).toBeInTheDocument();
+    expect(shareButton).toBeInTheDocument();
 
-  //   await waitFor(() => {
-  //     expect(fetch(url).toHaveBeenCalledTimes(0);
-  //   });
+    userEvent.click(startButton);
 
-  //   expect(await screen.findByTestId('start-recipe-btn')).toHaveTextContent('Start Recipe');
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/meals/52771/in-progress');
+    });
+  });
 
-  //   expect(history.location.pathname).toBe('/drinks');
-  // });
-  // it('Verifica se a imagem é renderizada na tela', () => {
-  //   const img = screen.getAllByTestId('recipe-photo');
-  //   expect(img).toBeInTheDocument();
-  // });
+  it('Verifica se o botão é rederizado na tela de drinks', async () => {
+    const urlDrink = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13501';
+    fetch(urlDrink);
+    const { history } = renderWithRouter(<App />, '/drinks/13501');
+
+    const startButton = screen.getByTestId('start-recipe-btn');
+
+    expect(startButton).toBeInTheDocument();
+
+    userEvent.click(startButton);
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/drinks/13501/in-progress');
+    });
+  });
+
+  it('', () => {
+
+  });
+});
+
+const SEARCH_TOP_BTN = 'search-top-btn';
+// const SEARCH_INPUT = 'search-input';
+// const NAME_SEARCH_RADIO = 'name-search-radio';
+// const EXEC_SEARCH_BTN = 'exec-search-btn';
+
+const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771';
+const testDoneRecipes = [{
+  id: '52771',
+  type: 'meal',
+  nationality: 'Italian',
+  category: 'Vegetarian',
+  alcoholicOrNot: '',
+  name: 'Spicy Arrabiata Penne',
+  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  doneDate: '22/6/2020',
+  tags: ['Pasta', 'Curry'],
+}];
+
+jest.useRealTimers();
+jest.setTimeout(30000);
+
+describe('Chamada de Api', () => {
+  afterEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(testDoneRecipes),
+    });
+  });
+  // afterEach(() => fetchMock.restore());
+
+  it.only('Verifica se api Meals é chamada', async () => {
+    const { history } = renderWithRouter(<App />, '/meals');
+
+    const btnSearch = screen.getByTestId(SEARCH_TOP_BTN);
+
+    fetchMock.mock(url, testDoneRecipes);
+
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(fetchMock.called(url)).toBeTruthy();
+    });
+
+    expect(history.location.pathname).toBe('/meals/52771');
+  });
 });
