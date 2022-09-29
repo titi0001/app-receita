f66b3cd8576f1f713dc883bd15bd13c6f39f4820
Thiago Pereira <titiura@gmail.com>
2022-09-29 09:28:28 -0300

    add teste pra passar no requesito 15 e crianção de mocks

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -5,9 +5,12 @@ import renderWithRouter from './RenderWithRouter';
 import App from '../App';
 
 const SEARCH_TOP_BTN = 'search-top-btn';
+const SEARCH_INPUT = 'search-input';
+const NAME_SEARCH_RADIO = 'name-search-radio';
+const EXEC_SEARCH_BTN = 'exec-search-btn';
 
 describe('Desenvolva testes para atingir cobertura total do Header', () => {
-  it('Verifica se e exibida a mensagem de alerta caso não encontre nenhuma receita', async () => {
+  it('Verifica se e exibida a mensagem de alerta caso não encontre nenhuma receita na tela de meals', async () => {
     renderWithRouter(<App />, '/meals');
 
     const btnSearch = screen.getByTestId(SEARCH_TOP_BTN);
@@ -18,14 +21,41 @@ describe('Desenvolva testes para atingir cobertura total do Header', () => {
       () => expect(screen.getByText(/search/i)),
     );
 
-    const inputSearch = screen.getByTestId('search-input');
+    const inputSearch = screen.getByTestId(SEARCH_INPUT);
     userEvent.type(inputSearch, 'xxxx');
 
-    const BtnRadioName = screen.getByTestId('name-search-radio');
+    const BtnRadioName = screen.getByTestId(NAME_SEARCH_RADIO);
     userEvent.type(BtnRadioName, { target: { value: true } });
     expect(BtnRadioName.value).toBe('name');
 
-    const btnSearchRecipes = screen.getByTestId('exec-search-btn');
+    const btnSearchRecipes = screen.getByTestId(EXEC_SEARCH_BTN);
+    expect(btnSearchRecipes).toBeInTheDocument();
+    userEvent.click(btnSearchRecipes);
+
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+    await waitFor(
+      () => expect(alertMock).toHaveBeenCalledTimes(1),
+    );
+  });
+  it('Verifica se e exibida a mensagem de alerta caso não encontre nenhuma receita na tela de drinks', async () => {
+    renderWithRouter(<App />, '/drinks');
+
+    const btnSearch = screen.getByTestId(SEARCH_TOP_BTN);
+    expect(btnSearch).toBeInTheDocument();
+    userEvent.click(btnSearch);
+
+    await waitFor(
+      () => expect(screen.getByText(/search/i)),
+    );
+
+    const inputSearch = screen.getByTestId(SEARCH_INPUT);
+    userEvent.type(inputSearch, 'xxxx');
+
+    const BtnRadioName = screen.getByTestId(NAME_SEARCH_RADIO);
+    userEvent.type(BtnRadioName, { target: { value: true } });
+    expect(BtnRadioName.value).toBe('name');
+
+    const btnSearchRecipes = screen.getByTestId(EXEC_SEARCH_BTN);
     expect(btnSearchRecipes).toBeInTheDocument();
     userEvent.click(btnSearchRecipes);
 
@@ -40,7 +70,7 @@ describe('Desenvolva testes para atingir cobertura total do Header', () => {
     const btnSearch = screen.getByTestId(SEARCH_TOP_BTN);
     expect(btnSearch).toBeInTheDocument();
     userEvent.click(btnSearch);
-    const inputSearch = screen.getByTestId('search-input');
+    const inputSearch = screen.getByTestId(SEARCH_INPUT);
     userEvent.type(inputSearch, 'abc');
     userEvent.clear(inputSearch);
   });
@@ -55,7 +85,7 @@ describe('Desenvolva testes para atingir cobertura total do Header', () => {
       () => expect(screen.getByText(/name/i)),
     );
 
-    const BtnRadioName = screen.getByTestId('name-search-radio');
+    const BtnRadioName = screen.getByTestId(NAME_SEARCH_RADIO);
     userEvent.type(BtnRadioName, { target: { value: true } });
     expect(BtnRadioName.value).toBe('name');
     userEvent.clear(BtnRadioName);
@@ -81,7 +111,7 @@ describe('Desenvolva testes para atingir cobertura total do Header', () => {
       () => expect(screen.getByText(/search/i)),
     );
 
-    const btnSearchRecipes = screen.getByTestId('exec-search-btn');
+    const btnSearchRecipes = screen.getByTestId(EXEC_SEARCH_BTN);
     expect(btnSearchRecipes).toBeInTheDocument();
     userEvent.click(btnSearchRecipes);
   });
